f46c051d84666969c785ea93cc8113ec3bc05a11
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2018-02-21 22:01:49 -0500

    Info pane transition.

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -7,27 +7,35 @@ class InfoPane extends React.Component {
 
     render() {
         // Render transit breadcrumbs when a response is ready.
-        if (this.props.route_lookup_response_status === "READY") {
-            const breadcrumbs = this.props.route_lookup_response.map((opt, i) =>
-                <TransitBreadcrumbContainer transit_option={opt} key={i} idx={i}/>
-            );
+        const duration = 200;
+        return (
+            <Transition in={this.props.route_lookup_response_status === "READY"} timeout={duration}>
+                {(animation_state) => {
+                    if (this.props.route_lookup_response_status !== "READY") {
+                        return null;
+                    } else {
+                        const breadcrumbs = this.props.route_lookup_response.map((opt, i) =>
+                            <TransitBreadcrumbContainer transit_option={opt} key={i} idx={i}/>
+                        );
 
-            let className = "info-pane";
-            // className += (this.props.route_selected_idx !== null) ? " info-pane-fadeout info-pane-post-fadeout" : "";
+                        const defaultStyle = {
+                            transition: `opacity ${duration}ms ease-in-out, left ${duration}ms ease-in-out`,
+                            opacity: 0,
+                        };
 
-            return (
-                <Transition timeout={0.66}>
-                    <div className={className}>
-                        <div className={"breadcrumbs-container"}>{breadcrumbs}</div>
-                    </div>
-                </Transition>
-            );
-        }
+                        const transitionStyles = {
+                            entering: {opacity: 0, left:'22%'},
+                            entered: {opacity: 1, left:'20%'}
+                        };
 
-        // Render nothing when this element is not necessary.
-        else {
-            return null;
-        }
+                        return <div className="info-pane"
+                                    style={{...defaultStyle, ...transitionStyles[animation_state]}}>
+                            <div className={"breadcrumbs-container"}>{breadcrumbs}</div>
+                        </div>
+                    }
+                }}
+            </Transition>
+        );
     }
 
 }
