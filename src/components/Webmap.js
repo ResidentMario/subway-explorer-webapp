4d92ccbfb546615b42f276226c5ca6fc4b7ce5cc
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2018-03-11 22:45:55 -0400

    Implement Webmap hider element.

--- src/components/Webmap.js
+++ src/components/Webmap.js
@@ -2,11 +2,13 @@ import React from "react";
 const Map = require('react-leaflet').Map;
 const TileLayer = require('react-leaflet').TileLayer;
 const Marker = require('react-leaflet').Marker;
+import { Transition } from 'react-transition-group';
 
 class Webmap extends React.Component {
 
     render() {
 
+        // Build the map.
         let tiles = <TileLayer
             attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -24,14 +26,27 @@ class Webmap extends React.Component {
             map_elements.push(<Marker position={[this.props.end_pin.y, this.props.end_pin.x]} key={3}/>)
         }
 
-        return (
-            <Map zoom={11}
+        // Build the hider element.
+        const defaultStyle = {
+            transition: `opacity ${duration}ms ease-in-out`,
+            opacity: 0,
+        };
+        const transitionStyles = {
+            entering: {opacity: 0},
+            entered: {opacity: 1}
+        };
+        const isin = (this.props.route_lookup_response_status === "READY");
+        const ispainted = (this.props.route_lookup_response_status === "READY");
+        const duration = 200;
+        let hider = <Transition in={isin} timeout={duration}>
+            {(animation_state) => ispainted ? <div className="hider" style={{...defaultStyle, ...transitionStyles[animation_state]}}/> : null}
+        </Transition>;
+
+        return [<Map zoom={11}
                  center={[40.713575, -73.967016]}
                  onClick={this.props.onClick}>
                 {map_elements}
-            </Map>
-        )
-
+        </Map>, hider];
     }
 }
 
--- src/containers/WebmapContainer.js
+++ src/containers/WebmapContainer.js
@@ -3,7 +3,10 @@ import Webmap from '../components/Webmap';
 import { sendPin } from '../actions';
 
 const mapStateToProps = (state, ownProps) => {
-    return {start_pin: state.route_selection.start_pin, end_pin: state.route_selection.end_pin};
+    return {
+        start_pin: state.route_selection.start_pin, end_pin: state.route_selection.end_pin,
+        route_lookup_response_status: state.route_selection.route_lookup_response_status
+    };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
